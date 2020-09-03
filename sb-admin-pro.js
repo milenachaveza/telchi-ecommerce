ee1f1ebf18fa878a5743954ef2a0f391d4d4a120
Milena Chavez <mca_bff@hotmail.com>
2020-09-02 20:51:51 -0400

    Update sb-admin-pro.js

--- sb-admin-pro.js
+++ sb-admin-pro.js
@@ -1,6 +1,6 @@
 /*!
     * Start Bootstrap - SB Admin Pro v1.2.0 (https://shop.startbootstrap.com/product/sb-admin-pro)
-    * Copyright 2013-2020 Start Bootstrap jb
+    * Copyright 2013-2020 Start Bootstrap jb kbj
     * Licensed under SEE_LICENSE (https://github.com/StartBootstrap/sb-admin-pro/blob/master/LICENSE)
     */
     (function($) {
